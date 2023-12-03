06e0983e5a190db2428b4c1b90c01952c116aba3
Developer-Krishnapc <krishna.p.choudhary.20@gmail.com>
2023-12-03 22:17:32 +0530

    images for the readme added

--- lib/main.dart
+++ lib/main.dart
@@ -80,6 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             ElevatedButton(
               child: const Text('Show Camera'),
